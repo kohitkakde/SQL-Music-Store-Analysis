24495cab06904d0826788047b81b9f801aac6c2a
KOHIT MANOJ KAKDE <156336818+kohitkakde@users.noreply.github.com>
2024-05-30 00:17:58 +0530

    Update Music Store Analysis.sql

--- Music Store Analysis.sql
+++ Music Store Analysis.sql
@@ -81,11 +81,6 @@ order by milliseconds  desc ;
 
 /* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
 
-/* Steps to Solve: First, find which artist has earned the most according to the InvoiceLines. Now use this artist to find 
-which customer spent the most on this artist. For this query, you will need to use the Invoice, InvoiceLine, Track, Customer, 
-Album, and Artist tables. Note, this one is tricky because the Total spent in the Invoice table might not be on a single product, 
-so you need to use the InvoiceLine table to find out how many of each product was purchased, and then multiply this by the price
-for each artist. */
 
 with best_selling_artist as
       (select a.artist_id artist_id , a.name artist_name ,   sum(i.unit_price*i.quantity) total_sales from invoice_line i
@@ -110,8 +105,6 @@ with best_selling_artist as
 with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where 
 the maximum number of purchases is shared return all Genres. */
 
-/* Steps to Solve:  There are two parts in question- first most popular music genre and second need data at country level. */
-
 
 with popular_genre as 
     ( select count(il.quantity) as purchase , c.country , g.name , g.genre_id,
@@ -129,9 +122,6 @@ with popular_genre as
    /* Q3: Write a query that determines the customer that has spent the most on music for each country. 
 Write a query that returns the country along with the top customer and how much they spent. 
 For countries where the top amount spent is shared, provide all customers who spent this amount. */
-
-/* Steps to Solve:  Similar to the above question. There are two parts in question- 
-first find the most spent on music for each country and second filter the data for respective customers. */
 
 
 with spent_on_music as (
